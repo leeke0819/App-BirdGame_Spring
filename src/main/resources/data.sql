ead16de4ecefbba060a7c207e5c12826d98e8a65
kimjoon <88149041+leeke0819@users.noreply.github.com>
2025-05-17 16:08:14 +0900

    2025 / 05 / 17

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO item_entity (item_code, item_description, item_name, image_root, price, type, user_id, is_egg, is_display) VALUES
-('egg_001', '흔히 뱁새로 알려진 오목눈이의 알입니다. 부화하면 귀여운 아기새를 볼 수 있을지도..?', '오목눈이 알', 'bird_egg_001.png', 30000, 100, NULL, 1, 0),
+INSERT INTO item_entity (item_code, item_description, item_name, image_root, price, type, is_egg, is_display) VALUES
+('egg_001', '흔히 뱁새로 알려진 오목눈이의 알입니다. 부화하면 귀여운 아기새를 볼 수 있을지도..?', '오목눈이 알', 'bird_egg_001.png', 30000, 100, 1, 0),
 
-('item_001', '잘 익은 사과입니다.', '사과', 'apple.png', 3000, 1, NULL, 0, 1),
-('item_002', '껍질이 있는 해바라기 씨앗입니다. 껍질이 있어, 아기새에게는 먹이기 힘들어 보입니다.', '껍질이 있는 해바라기 씨앗', 'sun_flower_seed_v1.png', 1000, 1, NULL, 0, 1),
-('item_003', '껍질이 없는 해바라기 씨앗입니다.', '껍질이 없는 해바라기 씨앗', 'sun_flower_seed_v2.png', 500, 1, NULL, 0, 1),
-('item_004', '껍질이 있는 땅콩입니다. 껍질이 있어, 아기새에게는 먹이기 힘들어 보입니다.', '껍질이 있는 땅콩', 'peanut_v1.png', 2500, 1, NULL, 0, 1),
-('item_005', '껍질이 없는 땅콩입니다.', '껍질이 없는 땅콩', 'peanut_v2.png', 2000, 1, NULL, 0, 1),
-('item_006', '잘 익은 귤입니다.', '귤', 'gamgyul.png', 4000, 1, NULL, 0, 1),
-('item_007', '잘 익은 포도입니다.', '포도', 'grape.png', 5000, 1, NULL, 0, 1);
+('item_001', '잘 익은 사과입니다.', '사과', 'apple.png', 3000, 1, 0, 1),
+('item_002', '껍질이 있는 해바라기 씨앗입니다. 껍질이 있어, 아기새에게는 먹이기 힘들어 보입니다.', '껍질이 있는 해바라기 씨앗', 'sun_flower_seed_v1.png', 1000, 1, 0, 1),
+('item_003', '껍질이 없는 해바라기 씨앗입니다.', '껍질이 없는 해바라기 씨앗', 'sun_flower_seed_v2.png', 500, 1, 0, 1),
+('item_004', '껍질이 있는 땅콩입니다. 껍질이 있어, 아기새에게는 먹이기 힘들어 보입니다.', '껍질이 있는 땅콩', 'peanut_v1.png', 2500, 1, 0, 1),
+('item_005', '껍질이 없는 땅콩입니다.', '껍질이 없는 땅콩', 'peanut_v2.png', 2000, 1, 0, 1),
+('item_006', '잘 익은 귤입니다.', '귤', 'gamgyul.png', 4000, 1, 0, 1),
+('item_007', '잘 익은 포도입니다.', '포도', 'grape.png', 5000, 1, 0, 1);
 
 
 
